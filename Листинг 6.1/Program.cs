7dddcbae1ce1302acd89d4bbb2d02d80f747704b
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-20 22:47:36 +0200

    Add files via upload

---
+++ Листинг 6.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass A = new myClass();
+            myClass B = new myClass();
+            
+            A.number = 123;
+            A.symbol = 'A';
+            B.number = 123;
+            B.symbol = 'B';
+
+            WriteLine("Първи обект");
+            A.show();
+            WriteLine("Втори обект");
+            B.show();
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+    class myClass
+    {
+        public int number;
+        public char symbol;
+
+        public void show()
+        {
+            WriteLine("Целочислено поле: " + number);
+            WriteLine("Символно поле: " + symbol);
+        }
+    }
+}
---
+++ Листинг 6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._2
+{
+    class myClass{
+        public int number;
+        public void show(){
+            WriteLine("Стойност на полето: "+number);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass A,B;
+            A = new myClass();
+            B = A;
+
+            A.number=123;
+            B.show();
+            B.number=321;
+            A.show();
+            
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
