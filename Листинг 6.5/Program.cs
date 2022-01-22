553b024db614c245c66a8643adb4c0de8e12afbf
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-22 20:34:36 +0200

    Add files via upload

---
+++ Листинг 6.5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._5
+{
+    class myClass
+    {
+        private string name;
+        public myClass(string txt)
+        {
+            name = txt;
+            WriteLine("Suzdaden e obektat\"{0}\"", name);
+        }
+        ~myClass()
+        {
+            Console.WriteLine("Iztrit e obektat\"{0}\"", name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void maker(string txt)
+            {
+                new myClass(txt);
+            }
+
+            myClass A = new myClass("Purvi");
+            new myClass("Vtori");
+            A = new myClass("Treti");
+            maker("Chetvurti");
+            A = new myClass("Peti");
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
