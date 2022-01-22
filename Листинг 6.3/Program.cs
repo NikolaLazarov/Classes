03f64cd94f09d9e3ff3e43b4eece2371c2240639
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-22 19:35:22 +0200

    Add files via upload

---
+++ Листинг 6.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._3
+{
+    class myClass
+    {
+        private int number;
+        private char symbol;
+        public void show()
+        {
+            WriteLine("Poletata na obekta: " + number + "и" + symbol);
+        }
+        public void set(int n, char s)
+        {
+            number = n;
+            symbol = s;
+        }
+        public void set(int n)
+        {
+            number = n;
+            symbol = 'B';
+        }
+        public void set()
+        {
+            set(100, 'A');
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+            obj.set();
+            obj.show();
+            obj.set(200);
+            obj.show();
+            obj.set(300, 'C');
+            obj.show();
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
