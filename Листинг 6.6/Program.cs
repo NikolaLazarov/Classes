17dac491de40178afbf63b753082d97a173721ad
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-23 00:01:37 +0200

    Add files via upload

---
+++ Листинг 6.6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._6
+{
+    class myClass
+    {
+        public static int code = 100;
+        public static void show()
+        {
+            WriteLine("Statichno  pole" + code);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass.show();
+            myClass.code = 200;
+            myClass.show();
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Листинг 6.7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._7
+{
+    class myMath
+    {
+        public const double Pi = 3.141592;
+        private static int N = 100;
+        public static double exp(double x)
+        {
+            double s = 0, q = 1;
+            for (int i = 0; i < N; i++)
+            {
+                s += q;
+                q *= x / (i + 1);
+            }
+            return s;
+        }
+        public static double sin(double x)
+        {
+            double s = 0, q = x;
+            for (int i = 0; i < N; i++)
+            {
+                s += q;
+                q *= (-1) * x * x / (2 * i + 2) / (2 * i + 3);
+            }
+            return s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z = 1;
+            WriteLine("sin({0})={1}", z, myMath.exp(z));
+            WriteLine("kontrolna stoinost: {0}", Math.Exp(z));
+            z = myMath.Pi / 4;
+            WriteLine("sin({0})={1}", z, myMath.sin(z));
+            WriteLine("kontrolna stoinost: {0}", Math.Sin(z));
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
