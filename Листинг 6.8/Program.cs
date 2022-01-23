37193ac8831df749a2d47c16962db24d4e4cce32
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-23 16:20:41 +0200

    Add files via upload

---
+++ Листинг 6.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._8
+{
+    class myClass
+    {
+        private int code;
+        public int get()
+        {
+            return this.code;
+        }
+        public void set(int code)
+        {
+            this.code = code;
+        }
+        public myClass(int code)
+        {
+            this.code = code;
+            WriteLine("Suzdaden e obektat: " + this.get());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass(100);
+            obj.set(200);
+            WriteLine("Nova stoinost: " + obj.get());
+
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Листинг 6.9/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Листинг_6._9
+{
+    class myClass{
+        public int alpha;
+        public int bravo;
+        public myClass(int a){
+            WriteLine("Konstruktor s edin argument");
+            alpha = a;
+            bravo=alpha;
+            WriteLine("I poleta sa ravni na"+alpha);
+        }
+        public myClass(int a, int b):this(a){
+            WriteLine("Konstruktor s dva argomenta");
+            bravo = b;
+            WriteLine("Poleta"+alpha+"i"+bravo);
+        }
+        public myClass():this(400,500){
+            WriteLine("Konstruktor bez argomenti");
+            WriteLine("Stoinost"+alpha+"i"+bravo);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass A = new myClass(100);
+            WriteLine();
+
+            myClass B  = new myClass(200,300);
+            WriteLine();
+
+            myClass C = new myClass();
+            WriteLine("Press any key to exit...");
+            ReadKey(true);
+        }
+    }
+}
