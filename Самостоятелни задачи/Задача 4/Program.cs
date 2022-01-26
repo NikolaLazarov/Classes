140286854969ad7da4aa4eb16df16335bf9c5520
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-26 19:43:32 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_4
+{
+    class myClass
+    {
+        public int a;
+        public char b;
+        public double c;
+        public myClass(double c)
+        {
+            int b = Convert.ToInt32(c);
+            this.b = (char)b;
+            WriteLine(this.b);
+            double d = b;
+            this.a = Convert.ToInt32((c - b) * 100);
+            WriteLine(this.a);
+        }
+        public myClass(int a, char b)
+        {
+            double d = (int)b;
+            double c = a;
+            this.c = d + (c / 100);
+            WriteLine(this.c);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass(89.2323);
+            myClass obj2 = new myClass(23, 'Y');
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Самостоятелни задачи/Задача 5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_5
+{
+    class myClass
+    {
+        public int a;
+        public void Meth()
+        {
+            this.a = 0;
+            WriteLine(this.a);
+        }
+        public void Meth(int a)
+        {
+            if (a >= 100)
+            {
+                this.a = 100;
+                WriteLine(this.a);
+            }
+        }
+        public myClass()
+        {
+            this.a = 0;
+            WriteLine(this.a);
+        }
+        public myClass(int a)
+        {
+            if (a >= 100)
+            {
+                this.a = 100;
+                WriteLine(this.a);
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                myClass obj = new myClass();
+                obj.Meth();
+                obj.Meth(101);
+                myClass obj2 = new myClass(101);
+
+                WriteLine("Press any key to close...");
+                ReadKey(true);
+            }
+        }
+    }
+}
+
