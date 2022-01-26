7b7ff79827df937113fd5424406f35b3aa6e205c
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-26 18:21:34 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_3
+{
+    class myClass
+    {
+        private int a;
+        private int b;
+        public myClass()
+        {
+            WriteLine("Конструктор без аргоменти.");
+        }
+        public myClass(int a)
+        {
+            this.a = a;
+            WriteLine("Конструктор с един аргомент. " + this.a);
+        }
+        public myClass(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            WriteLine("Конструктор с два аргомента. " + this.a + ", " + this.b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+            myClass obj1 = new myClass(7);
+            myClass obj2 = new myClass(7, 1);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
