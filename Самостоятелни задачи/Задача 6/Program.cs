cc05d8630a2793e10f48f67858ec3b951cb8d2bc
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-27 22:37:08 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 6/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_6
+{
+    class myClass
+    {
+        private int min = 4;
+        private int max = 45;
+        public void Meth(int x)
+        {
+            if (x > max)
+            {
+                min = max;
+                max = x;
+                WriteLine("Min: " + min);
+                WriteLine("Max: " + x);
+            }
+            else if (x < min)
+            {
+                min = x;
+                WriteLine("Min: " + x);
+                WriteLine("Max: " + max);
+            }
+        }
+        public void Meth(int x, int y)
+        {
+            if (x > y)
+            {
+                min = y;
+                max = x;
+                WriteLine("Min: " + min);
+                WriteLine("Max: " + max);
+            }
+            if (x < y)
+            {
+                min = x;
+                max = y;
+                WriteLine("Min: " + min);
+                WriteLine("Max: " + max);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+            obj.Meth(65);
+            obj.Meth(25);
+            obj.Meth(3, 9);
+            obj.Meth(7, 3);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
---
+++ Самостоятелни задачи/Задача 6v2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_6v2
+{
+    class myClass{
+        private int min = 4;
+        private int max = 45;
+
+        public myClass(int x){
+            if(x>max){
+                min = max;
+                max = x;
+                WriteLine("Min: " + min);
+                WriteLine("Max: " + x);
+            }else if(x<min){
+                min = x;
+                WriteLine("Min: " + x);
+                WriteLine("Max: "+ max);
+            }
+        }
+        public myClass(int x, int y){
+            if(x>y){
+                max = x;
+                min = y;
+                WriteLine("Min: " + min);
+                WriteLine("Max: " + max);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass(65);
+            myClass obj1 = new myClass(25);
+            myClass obj2 = new myClass(3,7);
+            myClass obj3 = new myClass(9,3);
+            
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
