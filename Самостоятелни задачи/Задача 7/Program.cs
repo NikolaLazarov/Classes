02b92001c6e11ed16f2b09628df2426731437bac
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-29 20:45:49 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Задача_7
+{
+    class myClass
+    {
+        private char x;
+        private string y;
+        public void Meth(char x)
+        {
+            this.x = x;
+            Console.WriteLine("{0}", this.x);
+        }
+        public void Meth(string y)
+        {
+            this.y = y;
+            Console.WriteLine("{0}", this.y);
+        }
+        public void Meth(char[] arrChars)
+        {
+            y = "";
+            if (arrChars.Length > 1)
+            {
+                arrChars.ToString();
+                foreach (char c in arrChars)
+                {
+                    y += c;
+                }
+                Console.WriteLine("{0}", y);
+            }
+            else
+            {
+                arrChars.ToString();
+                y += arrChars[0];
+                Console.WriteLine("{0}", y);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
