d13f527dc85ab10697a91225401de6a232cdecdf
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-29 23:19:48 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_10
+{
+    class myClass{
+        public void csc(int x){
+            WriteLine("{0}",Math.Cos(x));
+            WriteLine("{0}",Math.Sinh(x));
+            WriteLine("{0}",Math.Cosh(x));
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+            obj.csc(5);
+            
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
