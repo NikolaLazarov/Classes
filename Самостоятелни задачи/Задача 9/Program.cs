b00bbd263235e5d4e1e563f8e25c8b11334f0bc9
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-29 21:37:07 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+using System.Linq;
+
+namespace Задача_9
+{
+    class myClass
+    {
+        public static void Meth(params int[] arrNums)
+        {
+            WriteLine("Най-малкото число: {0}", arrNums.Min());
+            WriteLine("Най-голямото число: {0}", arrNums.Max());
+            WriteLine("Средно аритметично на чисата: {0}", arrNums.Average());
+        }
+        public static void Meth(int[] arrNums)
+        {
+            WriteLine("Най-малкото число: {0}", arrNums.Min());
+            WriteLine("Най-голямото число: {0}", arrNums.Max());
+            WriteLine("Средно аритметично на чисата: {0}", arrNums.Average());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Meth(6, 4, 5, 3, 2, 4, 9, 2);
+            int[] arrNums = { 3, 7, 2, 1, 8, 6, 2, 5 };
+            Meth(arrNums);
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
