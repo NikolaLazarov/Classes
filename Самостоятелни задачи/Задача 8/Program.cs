82ecd3f70cfb47456490bcc4ef9d6fb2260aff89
Nikola Lazarov <94927697+NikolaLazarov@users.noreply.github.com>
2022-01-29 21:24:00 +0200

    Add files via upload

---
+++ Самостоятелни задачи/Задача 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace Задача_8
+{
+    class myClass
+    {
+        private static int n = 0;
+        public void Meth()
+        {
+            WriteLine(n);
+            n++;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myClass obj = new myClass();
+            obj.Meth();
+            obj.Meth();
+            obj.Meth();
+
+            WriteLine("Press any key to close...");
+            ReadKey(true);
+        }
+    }
+}
